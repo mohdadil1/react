6a67ee02b96bb7a6768696d0920d3c62ec3a57ac
mohd adil <adilm925@gmail.com>
2024-06-07 11:30:45 +0530

    updated

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import Login from './components/Login'
 import Profile from'./components/Profile'
-import UserContextProvider from './Context/UserContextProvider'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   
--- miniContext/src/Context/UserContextProvider.jsx
+++ miniContext/src/Context/UserContextProvider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UserContext from './UserContext';
+import UserContext from '../context/UserContext'
 
 const UserContextProvider = ({children}) => {
     const [user, setUser] = React.useState(null)
